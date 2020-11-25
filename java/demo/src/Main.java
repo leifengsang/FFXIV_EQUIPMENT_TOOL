dc99006764a9118c7933f3a8f5fbc94d8f6f9b1a
leifengsang <575617231@qq.com>
2020-11-25 17:14:32 +0800

    do test

--- java/demo/src/Main.java
+++ java/demo/src/Main.java
@@ -1,5 +1,19 @@
+import java.util.List;
+
+import meta.Equipment;
+import model.EquipmemtModel;
+
 public class Main {
 	public static void main(String[] args) {
-
+		String name = "";
+		int length = 0;
+		List<Equipment> list = EquipmemtModel.getInstance().getEquipmentList();
+		for (Equipment equipment : list) {
+			if (equipment.getName().length() > length) {
+				length = equipment.getName().length();
+				name = equipment.getName();
+			}
+		}
+		System.out.println(name);
 	}
 }
